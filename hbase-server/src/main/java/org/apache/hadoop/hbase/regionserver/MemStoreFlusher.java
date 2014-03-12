2d2f346687bb99765fd2d92b1282790da089f3e3
iwasakims <iwasakims@example.com>
2014-03-12 07:20:23 -0700

    bumping the version of htrace to 3.0.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MemStoreFlusher.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MemStoreFlusher.java
@@ -49,9 +49,8 @@ import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.util.HasThread;
 import org.apache.hadoop.hbase.util.Threads;
 import org.apache.hadoop.util.StringUtils;
-import org.cloudera.htrace.Trace;
-import org.cloudera.htrace.TraceScope;
-import org.apache.hadoop.hbase.util.Counter;
+import org.htrace.Trace;
+import org.htrace.TraceScope;
 
 import com.google.common.base.Preconditions;
 
